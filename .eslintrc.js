ad34f4443ec609a51a9b41bf01688d570dd8b556
bichi <bichi@live.co.kr>
2017-09-26 05:42:55 +0900

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,52 @@
-// http://eslint.org/docs/user-guide/configuring
 module.exports = {
-    root: true,
-    parser: 'babel-eslint',
-    parserOptions: {
-        sourceType: 'module'
-    },
-    env: {
-        browser: true,
-        es6: true,
-        amd: true,
-    },
-    extends: ['eslint:recommended', 'google', 'vue'],
-    // required to lint *.vue files
-    plugins: [
-        'html',
-        'vue',
-    ],
-    'rules': {
-        'vue/jsx-uses-vars': 2,
-
-        'max-len': ['error', 140],
-
-        'one-var': 'off',
-
-        'no-console': 'off',
-
-        'indent': ['error', 2],
-
-        'comma-dangle': ['error', 'always-multiline'],
-
-        'space-before-blocks': ['error', 'never'],
-
-        'space-before-function-paren': ['error', 'never'],
-
-        'switch-colon-spacing': 'off',
-
-        'object-curly-spacing': ['error', 'never']
-    }
-}
+  parser: 'babel-eslint',
+  plugins: ['html', 'vue'],
+  env: {
+    'browser': true,
+    'es6': true,
+    'amd': true,
+  },
+  extends: ['eslint:recommended', 'google', 'vue'],
+  'parserOptions': {
+    'sourceType': 'module',
+  },
+  'rules': {
+    'vue/jsx-uses-vars': 2,
+    'max-len': ['error', 140],
+    'one-var': 'off',
+    'no-console': 'off',
+    indent: ['error', 2],
+    'linebreak-style': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    camelcase: 'off',
+    'space-before-function-paren': ['error', 'never'],
+    'object-curly-spacing': ['error', 'never'],
+    'keyword-spacing': ['error', {
+      before: false,
+      after: false,
+      overrides: {
+        'const' : {
+          before: true,
+          after: true,
+        },
+        from: {
+          before: true,
+          after: true,
+        },
+        'import': {
+          before: true,
+          after: true,
+        },
+        as: {
+          before: true,
+          after: true,
+        },
+        'return': {
+          before: true,
+          after: true,
+        }
+      }
+    }],
+    'space-before-blocks': ['error', 'never'],
+  }
+};
