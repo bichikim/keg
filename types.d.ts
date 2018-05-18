7dc55d1a5ea7c41d2f6f42167f4dae05c47e9b3f
BichiKim <bichi@live.co.kr>
2018-05-18 17:09:42 +0900

    update add new feature!  - ...keg({...}) will wrap actions at ones!

---
+++ types.d.ts
@@ -0,0 +1,26 @@
+import {ActionContext, Store} from 'vuex/types'
+import {Keg, sKeg, keg, kegRunner} from './src'
+import {
+  IKegOptions,
+  IPlugins,
+  IVuexKegOptions,
+  TAgedPlugin,
+  TInjectedFunction,
+  IAgedPlugins,
+  ActionHandler,
+} from './src/types'
+export {
+  Keg,
+  sKeg,
+  keg,
+  kegRunner,
+  ActionContext,
+  Store,
+  IKegOptions,
+  IPlugins,
+  IVuexKegOptions,
+  TAgedPlugin,
+  TInjectedFunction,
+  IAgedPlugins,
+  ActionHandler,
+}
